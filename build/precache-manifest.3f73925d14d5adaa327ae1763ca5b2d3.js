3c8cdb663d608d72f1175b565ecb4f6aecdd7bee
Armen Nersisyan <armenersisyan@gmail.com>
2020-07-12 10:35:05 +0400

    excluded node modules

---
+++ build/precache-manifest.3f73925d14d5adaa327ae1763ca5b2d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2c642ce583b6901e778902e475d79f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cc8db4da36e4c8d7942",
+    "url": "/static/css/main.3a3eb4c0.chunk.css"
+  },
+  {
+    "revision": "60cec44523dd945b9b5b",
+    "url": "/static/js/2.6a6e8db1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6a6e8db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc8db4da36e4c8d7942",
+    "url": "/static/js/main.cb05bbb0.chunk.js"
+  },
+  {
+    "revision": "f76bbb8b006909bb2d5d",
+    "url": "/static/js/runtime-main.b98ba16f.js"
+  },
+  {
+    "revision": "830453bb41eaf98d86555b8a11fee519",
+    "url": "/static/media/news.830453bb.svg"
+  },
+  {
+    "revision": "e0be4324ffc2983be391ebc8aef59aa3",
+    "url": "/static/media/not found.e0be4324.png"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/static/media/themify.e23a7dca.ttf"
+  }
+]);
